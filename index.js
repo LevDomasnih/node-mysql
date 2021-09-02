2f15d4bf9dc90b2791d4dbbc94fd9f9a1beb3220
LevDomasnih <lev.domasnix2013@gmail.com>
2021-09-03 00:41:06 +0500

    Настройка роутера

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
 const path = require('path')
+const todoRoutes = require('./routes/todo')
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api/todo', todoRoutes)
 
 app.use((req, res, next) => {
   res.sendFile('/index.html')
