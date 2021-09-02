0dd1176acfb368f57c5c9ce408ddd22c4b3dde95
LevDomasnih <lev.domasnix2013@gmail.com>
2021-09-03 01:25:11 +0500

    Подключение к базе данных

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
 const path = require('path')
+const sequelize = require('./utils/database')
 const todoRoutes = require('./routes/todo')
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -11,5 +12,13 @@ app.use('/api/todo', todoRoutes)
 app.use((req, res, next) => {
   res.sendFile('/index.html')
 })
+async function start() {
+  try {
+    await sequelize.sync()
+  } catch (e) {
+    console.log(e)
+  }
+}
 
+start()
 app.listen(PORT)
