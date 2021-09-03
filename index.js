cd86ca0bb7e1e4b2b7ff35fc6c38bda9aeb8d659
LevDomasnih <lev.domasnix2013@gmail.com>
2021-09-03 23:34:59 +0500

    Завершение POST-запроса

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const sequelize = require('./utils/database')
 const todoRoutes = require('./routes/todo')
 
 app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.json())
 app.use('/api/todo', todoRoutes)
 
 app.use((req, res, next) => {
